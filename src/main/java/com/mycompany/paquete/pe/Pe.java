cfd757c69d20dc805cc5ec93ccc63bafda7092c4
Karen <julieth.sanmiguel@espoch.edu.ec>
2024-11-25 14:58:10 -0500

    Helou

---
+++ src/main/java/com/mycompany/paquete/pe/Pe.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paquete.pe;
+
+/**
+ *
+ * @author DELL
+ */
+public class Pe {
+
+    public static void main(String[] args) {
+        System.out.println("Leonel");
+        System.out.println("de");
+        System.out.println("jesus");
+    }
+}
